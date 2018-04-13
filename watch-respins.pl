019b0a77d41d027152f4598aef66172d0e29572b
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-04-13 13:34:02 +0100

    Complete basic functionality

--- watch-respins.pl
+++ watch-respins.pl
@@ -5,18 +5,29 @@
 # Checks for changes to torrent files
 # If new torrent files are present, it downloads them to a specified location
 
+use 5.10.0;
 use strict;
 use warnings;
 use Data::Dumper;
 use WWW::Mechanize;
 use File::Fetch;
 
+# Time out after 30 seconds if a download doesn't succeed
+$File::Fetch::TIMEOUT = 30;
+
+my $release = "F27";
 my $respins_url = "http://dl.fedoraproject.org/pub/alt/live-respins/";
 my $torrent_dir = "/home/asinha/Downloads/torrent_temps/rtorrent_watch/";
+my @current_files = ();
+my $download_dir;
+# Select what flavours to download
+my @flavours = (qr/CINN/, qr/KDE/, qr/LXDE/, qr/LXQT/, qr/MATE/, qr/SOAS/, qr/WORK/, qr/XFCE/ );
 
 if (-d $torrent_dir)
 {
     print("Downloading to $torrent_dir\n");
+    opendir $download_dir, $torrent_dir or die "Cannot open directory: $!";
+    @current_files = readdir $download_dir;
 }
 else
 {
@@ -27,32 +38,38 @@ else
 my $mech = WWW::Mechanize->new();
 $mech->get( $respins_url );
 
-my $lives_date = "";
-
 if ($mech->success()) {
     my @all_links = $mech->links();
+
     foreach my $link (@all_links)
     {
-        if ( $link->url =~ m/CHECKSUM512-(\d{8})/ )
+        if ( $link->url =~ m/\.torrent$/ and $link->url ~~ @flavours)
         {
-            $lives_date = $1;
-            print("Current version of respins is: $lives_date\n");
+            $link->url =~ m/$release-(.*)-x86_64-(\d{8})\.torrent$/;
+            my $flavour = $1;
+            my $remote_version = $2;
+            my $local_version = "";
+            print("Checking requested flavour: $flavour\n");
 
-            # TODO check if we've already downloaded these
-            # my $file_loc = $respins_url.$link;
-            # print("Downloading: $file_loc to $torrent_dir\n");
-            # my $fetcher = File::Fetch->new(uri => $file_loc);
-            # my $where = $fetcher->fetch(to => $torrent_dir);
+            my $local_version = "0";
+            foreach my $current_file (@current_files)
+            {
+                if ($current_file =~ m/$release-$flavour-x86_64-(\d{8})/ )
+                {
+                    $local_version = $1;
+                }
+            }
+            if ($local_version ne $remote_version)
+            {
+                print("Local version ($local_version) is different from remote version ($remote_version). Downloading\n");
+                my $file_loc = $respins_url.$link->url;
+                print("Downloading: $file_loc to $torrent_dir\n");
+                my $fetcher = File::Fetch->new(uri => $file_loc);
+                my $where = $fetcher->fetch(to => $torrent_dir);
+            }
         }
-
-        # if ( $link =~ m/\.torrent$/ )
-        # {
-            # my $file_loc = $respins_url.$link;
-            # print("Downloading: $file_loc to $torrent_dir\n");
-            # my $fetcher = File::Fetch->new(uri => $file_loc);
-            # my $where = $fetcher->fetch(to => $torrent_dir);
-        # }
     }
+    closedir $download_dir;
 }
 else
 {
